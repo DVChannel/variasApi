21946c4819b6e30433325d07ddb963e22538ad07
DVChannel <channelgonzalezjimenez100gmail.com>
2020-09-14 23:36:56 -0500

    v1.4

---
+++ js/04-app.js
@@ -0,0 +1,10 @@
+const abrirBn = document.querySelector('abrir-pantalla-completa');
+const salirBn = document.querySelector('salir-pantalla-completa');
+
+
+salirBn.addEventListener('click', cerrarPantallaCompleta);
+abrirBn.addEventListener('click', pantallaCompleta);
+
+
+function pantallaCompleta(){document.documentElement.requestFullscreen();}
+function cerrarPantallaCompleta(){document.exitFullscreen();}
